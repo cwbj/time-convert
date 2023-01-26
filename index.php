42903b8afc45389fa1335a7ab59c71e2a500b3ed
zeneight <thezeneight@gmail.com>
2023-01-26 22:32:07 +0800

    feat: add functionality

--- index.php
+++ index.php
@@ -20,86 +20,71 @@
 </head>
 
 <body>
-
-
 	<div class="content">
-
 		<div class="container">
 			<div class="row">
 				<div class="col-md-5 mr-auto">
-					<h3 class="mb-3">Let's work together</h3>
-					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Recusandae sequi, ipsa hic alias
-						officia facilis eveniet, neque laborum cumque maxime soluta. Neque atque necessitatibus ipsam
-						sequi soluta magni, iste vero fuga inventore, explicabo totam quis quia nemo possimus cupiditate
-						doloribus?</p>
-					<p>Quasi optio numquam pariatur amet laudantium, dicta ullam obcaecati nihil eveniet et aperiam
-						beatae illum vitae in asperiores, a ex. Voluptates accusantium, beatae dolorem natus delectus
-						nostrum, iusto nemo optio quas eum, quis a in reprehenderit totam, sequi distinctio impedit.</p>
-					<p>Dolor aliquid, commodi vitae dolorum veniam amet error! Nemo nam quibusdam delectus dolore
-						eveniet officia harum eum sit magnam possimus. Eum optio dolores repellat ea impedit, in unde,
-						rem ipsam rerum autem iusto vero facilis blanditiis explicabo sapiente consequatur provident? <a
-							href="#">info@mywebsite.com</a></p>
-
+					<h3 class="mb-3">Time Convert App</h3>
+					<p>This App used to convert the inserted time to the other countries' local time. You only need to input
+						it on the form and choose the local time that you want to convert.</p>
+					<p>This app is still in BETA and everyone could make some suggest on the app.</p>
 				</div>
+
+				<?php
+				// php variable
+				// $timezones = array(
+				// 	'Asia/Kolkata'      => "(GMT+05:30) Kolkata/India",
+				// 	'Asia/Colombo'      => "(GMT+05:30) Colombo/Sri Lanka",
+				// 	'Asia/Kathmandu'    => "(GMT+05:45) Kathmandu/Nepal",
+				// 	'Asia/Yangon'       => "(GMT+06:30) Yangon/Myanmar",
+				// 	'Asia/Makassar'     => "(GMT+08:00) Makassar/Bali",
+				// 	'Asia/Manila'       => "(GMT+08:00) Manila/Philippines",
+				// 	'Asia/Tokyo'        => "(GMT+09:00) Tokyo/Japan"
+				// 	);
+
+				$timezones = array(
+					'Asia/Kolkata'      => "Kolkata/India",
+					'Asia/Colombo'      => "Colombo/Sri Lanka",
+					'Asia/Kathmandu'    => "Kathmandu/Nepal",
+					'Asia/Yangon'       => "Yangon/Myanmar",
+					'Asia/Makassar'     => "Makassar/Bali",
+					'Asia/Manila'       => "Manila/Philippines",
+					'Asia/Tokyo'        => "Tokyo/Japan"
+					);
+				?>
 
 				<div class="col-md-6">
 					<div class="box">
-						<h3 class="heading">How Can We Help?</h3>
-						<form class="mb-5" method="post" id="contactForm" name="contactForm">
-							<div class="row">
-
-								<div class="col-md-6 form-group">
-									<label for="name" class="col-form-label">Name *</label>
-									<input type="text" class="form-control" name="name" id="name"
-										placeholder="Your name">
-								</div>
-								<div class="col-md-6 form-group">
-									<label for="name" class="col-form-label">Organization</label>
-									<input type="text" class="form-control" name="organization" id="organization"
-										placeholder="Your organization">
-								</div>
-							</div>
+						<form class="mb-5" method="post" name="timeApp">
 							<div class="row">
 								<div class="col-md-12 form-group">
-									<label for="email" class="col-form-label">Email *</label>
-									<input type="text" class="form-control" name="email" id="email"
-										placeholder="Your email address">
+									<label for="time" class="col-form-label">Time *</label>
+									<input required type="time" class="form-control" name="date" id="time" value="<?php echo !empty($_POST['date']) ? $_POST['date'] : '' ?>">
 								</div>
 							</div>
 
-							<div class="row">
-								<div class="col-md-12 form-group">
-									<label for="message" class="col-form-label">Project Description *</label>
-									<textarea class="form-control" name="message" id="message" cols="30"
-										rows="7"></textarea>
-								</div>
-							</div>
 							<div class="row mb-3">
-								<div class="col-md-6 form-group">
-									<label for="budget" class="col-form-label">Budget</label>
-									<select class="custom-select" id="budget" name="budget">
+								<div class="col-md-12 form-group">
+									<label for="budget" class="col-form-label">Location Zone *</label>
+									<select required class="custom-select" id="zone" name="zone">
 										<option selected>Choose...</option>
-										<option value="$1000 below">
-											< $1,000</option> <option value="$2,000 - $5,000">$2,000 - $5,000
-										</option>
-										<option value="$5,000 - $15,000">$5,000 - $15,000</option>
-										<option value="$15,000 - $25,000">$15,000 - $25,000</option>
-										<option value="$25,000 >">$25,000 ></option>
+										<?php
+										foreach($timezones as $key => $value){
+											if(isset($_POST['zone']) && $_POST['zone']==$key) {
+												echo "<option selected name=\"zone\" value=\"$key\">$value</option>";
+											} else {
+												echo "<option name=\"zone\" value=\"$key\">$value</option>";
+											}
+												
+										}
+										?>
 									</select>
-								</div>
-								<div class="col-md-6 form-group">
-									<label for="message" class="col-form-label">Target Lunch</label>
-									<input type="text" class="form-control" name="date" id="date"
-										placeholder="Example: January 15th">
-
-
 								</div>
 							</div>
 
 							<div class="row">
 								<div class="col-md-12">
-									<input type="submit" value="Send Message"
-										class="btn btn-block btn-primary rounded-0 py-2 px-4">
+									<input type="submit" value="Submit" class="btn btn-block btn-primary rounded-0 py-2 px-4">
 									<span class="submitting"></span>
 								</div>
 							</div>
@@ -114,13 +99,71 @@
 			</div>
 		</div>
 
-
-
 		<script src="js/jquery-3.3.1.min.js"></script>
 		<script src="js/popper.min.js"></script>
 		<script src="js/bootstrap.min.js"></script>
 		<script src="js/jquery.validate.min.js"></script>
 		<script src="js/main.js"></script>
+
+		<?php
+		if(isset($_POST['date']) && isset($_POST['zone'])) {
+			try {
+				$data = [];
+				foreach($timezones as $key => $value){
+						$data[$key]['test'] = new DateTime($_POST['date'], new DateTimeZone($_POST['zone']));
+						$data[$key]['test']->setTimezone(new DateTimeZone($key));
+
+						$data[$key]['result'] = $data[$key]['test']->format('h:i A');
+						$data[$key]['key'] = $key;
+						$data[$key]['title'] = $value;
+				}
+				
+				// $result = $hongkong->format('h:i A');
+			} catch (Exception $e) {
+				echo $e->getMessage();
+			}
+		}
+
+		if(isset($_POST['date']) || isset($_POST['zone'])) {
+
+			echo "<script>
+			$(window).on('load', function(){
+					$('#thankyouModal').modal('show');
+				});
+			</script>";
+
+			$waktu = date('h:i A', strtotime($_POST['date']));
+		?>
+
+		<div class="modal fade" id="thankyouModal" tabindex="-1" role="dialog">
+			<div class="modal-dialog">
+				<div class="modal-content">
+					<div class="modal-header">
+						<h4 class="modal-title" id="myModalLabel"><?php echo $_POST['zone']. " (".$waktu.")"; ?></h4>
+						<button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+					</div>
+					<div class="modal-body">
+					<?php
+					foreach($data as $value){
+						if($_POST['zone']!=$value['key']) {
+							echo "<div class='row'><div class='col-md-6'>". $value['title']. "</div><div class='col-md-2'> <small><input type='text' value='" .$value['result'] ."'></small></div>";
+							echo "</div>";
+						}
+					}
+					?>
+					</div>
+				</div>
+			</div>
+		</div>
+
+		<?php
+		}
+		?>
+		
+
+
+
+		
 
 </body>
 
